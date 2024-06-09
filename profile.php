b3a9c6c1f7ef2bcf992f6ec3dc61f134ff513092
James <crawleydesign@hotmail.com>
2024-06-09 07:38:59 -0500

    Added ability to upload profile picture

--- profile.php
+++ profile.php
@@ -20,14 +20,28 @@ $write = '';
 $tools = '';
 
 if($_SESSION['userlevel'] == 'admin' || $_SESSION['userlevel'] == 'reviewer'){
-    $write = "<p><a href='write.php?id=" . $_SESSION['userid'] . "'>Write Review/Article</a></p>";
+    $write = "<p><a href='write.php?id=" . $_SESSION['userid'] . "'><span class='material-symbols-outlined'>edit_note</span>&nbsp;&nbsp;Write Review/Article</a></p>";
 
     if($_SESSION['userlevel'] == 'admin'){
-        $tools = "<p><a href='admin/tools.php' >Admin tools</a></p>";
+        $tools = "<p><a href='admin/tools.php' ><span class='material-symbols-outlined'>construction</span>&nbsp;&nbsp;Admin tools</a></p>";
     }
 }
 
 
+
+if(isset($_POST['sub-pic'])){
+    $filename = $_FILES['uploadimg']['name'];
+    $tempname = $_FILES['uploadimg']['tmp_name'];
+    $folder = "./profile-imgs/" . $filename;
+
+    move_uploaded_file($tempname, $folder);
+
+    $picSQL = "UPDATE users SET u_pic = ? WHERE u_id = " . $_SESSION['userid'];
+    $picStmt = $conn->prepare($picSQL);
+    $picStmt->bind_param('s', $folder);
+    $picStmt->execute();
+
+}
 
 ?>
 
@@ -49,7 +63,6 @@ if($_SESSION['userlevel'] == 'admin' || $_SESSION['userlevel'] == 'reviewer'){
         <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
         <link rel=”stylesheet” href=”https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css”>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
         
     </head>
     <body>
@@ -57,21 +70,26 @@ if($_SESSION['userlevel'] == 'admin' || $_SESSION['userlevel'] == 'reviewer'){
        <div class="wrapper">
             <?php include "includes/main-nav.php"; ?>
             <div class="profile">
-                    <div class="update-pic">
+                    <div class="update-pic" style='text-align'>
                         
                         <div class="prof-pic" style="background-image: url('<?php echo $profpic; ?>');" ></div>
                         <form action='profile.php' method='post' enctype='multipart/form-data' style='margin: 0; margin-top: 1rem; padding: 0; display: inline-block;'>
                         
-                        <label for='picbtn' class='profbtn'>Update Profile Pic</label><input type='file' class='picbtn' id='picbtn' />
+                        <p style='margin: 0; padding:0; text-align: center;'>
+                            <label for='picbtn' class='profbtn upload' >Upload Pic</label>
+                            <input type='file' name='uploadimg' class='picbtn' id='picbtn' onchange='uploadFunc()'/>
+                        </p>
+                        <p style='margin: 0; margin-top: 2rem; padding:0; text-align: center;'><span id='file-selected'></span></p>
+                        <p style='margin: 0; margin-top: 2rem; padding:0; text-align: center;'><input type='submit' name='sub-pic' class='profbtn upload-sub' id='upload-sub' value='Update'/></p>
                         </form>
                     </div>
                     <div class="prof-text">
                         <?php echo $loginErr; ?>
                         <h2><?php echo " " . $profRow['u_username']; ?></h2>
                         <h3><?php echo " " . $profRow['u_level']; ?></h3>
-                        <p><b>Email:</b> <?php echo $profRow['u_email'];  ?></p>
+                        <p><?php echo $profRow['u_email'];  ?></p>
                         <p>
-                            <a href="resetpassword.php" >Reset Password</a>
+                            <a href="resetpassword.php"><span class="material-symbols-outlined">lock_reset</span>&nbsp;&nbsp;Reset Password</a>
                         </p>
                         <?php 
                             echo $write;
@@ -93,5 +111,21 @@ if($_SESSION['userlevel'] == 'admin' || $_SESSION['userlevel'] == 'reviewer'){
     };
     </script>
         <script src="./scripts/navigation.js" defer></script>
+        <script defer>
+            const uploadimg = document.getElementById('picbtn');
+            const uploadsub = document.getElementById('upload-sub');
+
+            function uploadFunc(event){
+
+                if(uploadimg.value.length > 0){
+                    uploadsub.style.display = 'block';
+                    document.getElementById('file-selected').innerHTML = uploadimg.files[0].name;
+                }
+            }
+
+            
+
+
+        </script>
     </body>
 </html>
